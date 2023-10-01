7e06eff9061e135680930abcaad450583e41bb3c
IgorVdO <128544275+IgorVdO@users.noreply.github.com>
2023-10-01 20:09:06 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function clicada() {
+    const igor = document.getElementById('igor');
+    const ana = document.getElementById('ana');
+    igor.classList.add("grudarigor");
+    ana.classList.add("grudarana");
+}
